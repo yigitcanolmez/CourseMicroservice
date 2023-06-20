4b077b5f4db370f0af29184429156d5c23729e4c
Yigitcan Olmez <Yigitcan.Olmez@d-teknoloji.com.tr>
2023-06-20 20:29:20 +0300

    automapper implemented / appsettings configured

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.DTOs;
+using FreeCourse.Services.Catalog.Models;
+
+namespace FreeCourse.Services.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            #region Category
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+
+            #endregion
+
+            #region Feature
+            CreateMap<Feature, FeatureDTO>().ReverseMap();
+
+            #endregion
+
+            #region Course
+            CreateMap<Course, CourseCreateDTO>().ReverseMap();
+            CreateMap<Course, CourseUpdateDTO>().ReverseMap();
+            CreateMap<Course, CourseDTO>().ReverseMap();
+
+            #endregion
+        }
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,8 +1,21 @@
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddControllers();
+
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
