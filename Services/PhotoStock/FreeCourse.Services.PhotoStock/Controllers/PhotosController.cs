c876b0a74de7ccc07eae447f62fcd08aca98a4a4
Yigitcan Olmez <Yigitcan.Olmez@d-teknoloji.com.tr>
2023-06-25 18:25:59 +0300

    PhotoStockApı added

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,52 @@
+﻿using FreeCourse.Services.PhotoStock.DTOs;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream, cancellationToken);
+
+                    var returnPath = "photo/" + photo.FileName;
+
+                    PhotoDto photoDto = new()
+                    {
+                        Url = returnPath
+                    };
+                    return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, StatusCodes.Status200OK));
+                }
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("url is empty", StatusCodes.Status400BadRequest));
+
+        }
+
+        [HttpDelete]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("url is empty", StatusCodes.Status404NotFound));
+
+            }
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(StatusCodes.Status204NoContent));
+
+
+        }
+    }
+}
--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -1,27 +1,22 @@
-using FreeCourse.Services.Catalog.DependencyContainer;
-using FreeCourse.Services.Catalog.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.Extensions.Options;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-builder.Services.AddContainerServices(builder.Configuration);
-
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerURL"];
+    opt.Audience = "photo_stock_catalog";
+    opt.RequireHttpsMetadata = false;
+});
 builder.Services.AddControllers(opt =>
 {
     opt.Filters.Add(new AuthorizeFilter());
 });
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
- 
 
 var app = builder.Build();
 
@@ -31,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseStaticFiles();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
