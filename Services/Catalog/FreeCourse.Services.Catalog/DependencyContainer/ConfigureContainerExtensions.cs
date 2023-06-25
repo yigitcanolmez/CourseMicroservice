dab29dae7cb8af2f7689d638924b8f51f40b350c
Yigitcan Olmez <Yigitcan.Olmez@d-teknoloji.com.tr>
2023-06-25 15:55:19 +0300

    refactor

--- Services/Catalog/FreeCourse.Services.Catalog/DependencyContainer/ConfigureContainerExtensions.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/DependencyContainer/ConfigureContainerExtensions.cs
@@ -1,6 +1,7 @@
-﻿ using FreeCourse.Services.Catalog.Services.CategoryServices;
+﻿using FreeCourse.Services.Catalog.Services.CategoryServices;
 using FreeCourse.Services.Catalog.Services.CourseServices;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using System.Reflection;
 
@@ -10,6 +11,12 @@ namespace FreeCourse.Services.Catalog.DependencyContainer
     {
         public static IServiceCollection AddContainerServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = configuration["IdentityServerURL"];
+                opt.Audience = "resource_catalog";
+                opt.RequireHttpsMetadata = false;
+            });
             #region Database Settings
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
@@ -21,9 +28,10 @@ namespace FreeCourse.Services.Catalog.DependencyContainer
                 return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
             });
             #endregion
-            
-            services.AddScoped<ICategoryService,CategoryService>();
-            services.AddScoped<ICourseService,CourseService>();
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<ICourseService, CourseService>();
+
 
             return services;
         }
--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,6 +1,7 @@
 using FreeCourse.Services.Catalog.DependencyContainer;
 using FreeCourse.Services.Catalog.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 using System.Reflection;
 
@@ -9,16 +10,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddContainerServices(builder.Configuration);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-
-});
+ 
 
 var app = builder.Build();
 
@@ -28,7 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
