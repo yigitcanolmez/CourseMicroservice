a4b07b9ebf5c1e6de9b56e5566806d4aebd3b590
Yigitcan Olmez <Yigitcan.Olmez@d-teknoloji.com.tr>
2023-07-10 16:02:48 +0300

    command, commandhelper

---
+++ Services/Order/FreeCourse.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using FreeCourse.Services.Order.Application.Commands;
+using FreeCourse.Services.Order.Application.DTOs;
+using FreeCourse.Services.Order.Domain.OrderAggregate;
+using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Handlers
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDTO>>
+    {
+        private readonly OrderDbContext _dbContext;
+
+        public CreateOrderCommandHandler(OrderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Response<CreatedOrderDTO>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var detailAddress = request.AddressDTO;
+            var newAddress = new Address(
+                detailAddress.Province,
+                detailAddress.District,
+                detailAddress.Street,
+                detailAddress.ZipCode,
+                detailAddress.Line);
+
+            Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(newAddress, request.BuyerId);
+            request.OrderItems.ForEach(x =>
+            {
+                newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureURL);
+            });
+
+            _dbContext.Orders.Add(newOrder);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Response<CreatedOrderDTO>.Success(new CreatedOrderDTO { OrderId = newOrder.Id }, StatusCodes.Status200OK);
+
+        }
+    }
+}
