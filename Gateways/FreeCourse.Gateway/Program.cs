2e6d2d1212bd074334455272cd21e8a539cee632
Yigitcan Olmez <Yigitcan.Olmez@d-teknoloji.com.tr>
2023-07-11 22:41:23 +0300

    gateway implemented

--- Gateways/FreeCourse.Gateway/Program.cs
+++ Gateways/FreeCourse.Gateway/Program.cs
@@ -1,5 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Configuration
+//    .SetBasePath(builder.Environment.ContentRootPath.ToLower())
+//    .AddJsonFile("appsettings.json", true, true)
+//    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName.ToLower()}.json", true, true)
+//    .AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json")
+//    .AddEnvironmentVariables();
+
+builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json")
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot();
+
+
+
 var app = builder.Build();
+
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
