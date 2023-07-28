1a62cf13ee2c0650df3e3ec8ea77a96cb30fa40a
Yigitcan Olmez <Yigitcan.Olmez@d-teknoloji.com.tr>
2023-07-28 09:45:41 +0300

    rabbitmq / masstransit

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Models/OrderDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace FreeCourse.Services.FakePayment.Models
+{
+    public class OrderDTO
+    {
+        public OrderDTO()
+        {
+            OrderItems = new List<OrderItemDTO>();
+        }
+        public string BuyerId { get; set; }
+        public List<OrderItemDTO> OrderItems { get; set; }
+        public AddressDTO Address { get; set; }
+
+    }
+    public class AddressDTO
+    {
+        public string Province { get; private set; }
+        public string District { get; private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Line { get; private set; }
+
+    }
+    public class OrderItemDTO
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
---
+++ Shared/FreeCourse.Shared/Messages/Publisher/CreateOrderMessageCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.Messages.Publisher
+{
+    public class CreateOrderMessageCommand
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public Address Address { get; set; }
+
+
+    }
+    public class OrderItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public Decimal Price { get; set; }
+    }
+    public class Address
+    {
+        public string Province { get; private set; }
+        public string District { get; private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Line { get; private set; }
+    }
+}
